19963d042abedde4dbe6d99842d69d6a541dff79
Michael Kalantar <kalantar@us.ibm.com>
2021-02-05 14:35:02 -0500

    more cleanup

--- controllers/experiment_controller_test.go
+++ controllers/experiment_controller_test.go
@@ -27,9 +27,6 @@ import (
 
 var _ = Describe("Experiment Validation", func() {
 	ctx := context.Background()
-	// BeforeEach(func() {
-	// 	k8sClient.DeleteAllOf(ctx, &v2alpha1.Experiment{})
-	// })
 
 	Context("When creating an experiment with an invalid spec.duration.maxIteration", func() {
 		testName := "test-invalid-duration"
@@ -120,9 +117,6 @@ var _ = Describe("Experiment Validation", func() {
 
 var _ = Describe("Experiment proceeds", func() {
 	ctx := context.Background()
-	// BeforeEach(func() {
-	// 	k8sClient.DeleteAllOf(ctx, &v2alpha1.Experiment{})
-	// })
 
 	Context("Early event trigger", func() {
 		testName := "early-reconcile"
